19205ef2e78f7131a934337cbc6c654986ad0c32
Nour REBIAI <nourrebiai05@gmail.com>
2024-03-19 13:37:39 +0100

    updated code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+// import {  useState } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 import WorkoutDetails from "../Components/WorkoutDetails"
+import WorkoutForm from "../Components/WorkoutForm"
 
 const Home = () => {
-  const [workouts,setWorkouts]=useState(null)
+  // const [workouts,setWorkouts]=useState(null)
+  const {workouts,dispatch} =useWorkoutsContext()
   useEffect(()=>{
     const fetchWorkouts = async ()=>{
       const response = await fetch('/api/workouts')
       const json= await response.json()
 
       if (response.ok){
-         setWorkouts(json)
+        //  setWorkouts(json)
+        dispatch({type: 'SET_WORKOUT' , payload:json})
       }
-      
     }
     fetchWorkouts()
-  },[])
+  },[dispatch])
   return (
-    <div className='home  '>
+    <div className='home'>
        <div className="workouts">
         {workouts && workouts.map((workout)=>(
           <WorkoutDetails key={workout._id} workout={workout}/>
         ))}
        </div>
+       <WorkoutForm/>
     </div>
   )
 }
